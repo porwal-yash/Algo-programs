29cd6a51058382f3881cf14264309421985adf3d
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-10-03 19:12:25 +0530

    :fire: added and updated the wrong binary search code

--- binarySearch.c
+++ binarySearch.c
@@ -21,10 +21,10 @@ int binarySearch(int array[], int x, int low, int high) {
 }
 
 int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
-  int n = sizeof(array) / sizeof(array[0]);
+  int arr[] = {3, 4, 5, 6, 7, 8, 9};
+  int n = sizeof(arr) / sizeof(arr[0]);
   int x = 4;
-  int result = binarySearch(array, x, 0, n - 1);
+  int result = binarySearch(arr, x, 0, n - 1);
   if (result == -1)
     printf("Not found");
   else
